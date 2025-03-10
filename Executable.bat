859490b844af1907e74b5df9c417b9aa520d6b11
vergrey <verggrey3@gmail.com>
2025-03-10 14:58:20 +0300

    Delete Executable.bat

--- Executable.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Указание заголовка окна
-title Matrix
-:: Цвет текста
-color 02
-:start
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
-goto start
